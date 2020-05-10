39fa6438963dc957ee2c18fef84b034a42ae2225
Ademar Alves de Oliveira <ademar111190@gmail.com>
2020-05-09 21:27:04 -0300

    Some default apps for file types

--- defaults.sh
+++ defaults.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+# Desktop Environment
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# File types,
+# File type: mdls file.type | grep kMDItemContentType
+# App key: mdls App.app | grep kMDItemCFBundleIdentifier
+function setDefaultApp() {
+    app="$1"
+    shift
+    for fileType in "${@}"; do
+        defaults write ~/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist LSHandlers -array-add "{LSHandlerContentType=$fileType;LSHandlerRoleAll=$app;}"
+        defaults write ~/Library/Preferences/com.apple.LaunchServices.plist LSHandlers -array-add "{LSHandlerContentType=$fileType;LSHandlerRoleAll=$app;}"
+    done
+}
+
+setDefaultApp "com.sublimetext.3" \
+    "public.plain-text" \
+    "public.python-script" \
+    "net.daringfireball.markdown" \
+    "dyn.ah62d4rv4ge80442"
+
+setDefaultApp "com.aone.keka" \
+    "public.zip-archive"
 
 killall Finder
